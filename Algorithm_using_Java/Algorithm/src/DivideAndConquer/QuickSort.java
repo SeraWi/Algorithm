8ecb9566dae5a112bfdb8ef3f8a1cbdda2c797a5
SeraWi <wsr2357@naver.com>
2022-01-09 15:50:18 +0900

    Divide and Conquer : QuickSort

---
+++ Algorithm_using_Java/Algorithm/src/DivideAndConquer/QuickSort.java
@@ -0,0 +1,44 @@
+package DivideAndConquer;
+
+
+
+/*
+Divide and Conquer : QuickSort
+The size of two problems are flexible.
+Use a pivot
+Privot : an object to split the given sequence to two subsequences.
+1) Compare all numbers with the numbers at the pivot position
+2) Move all smaller numbers to left side of the pivot oand larger numbers to right
+side of the pivot
+3) Repeat this process for all subproblems recursively
+
+피벗 선정 후
+큰숫자는 피벗 기준 오른쪽, 작은 숫자는 왼쪽으로 나누기
+이방법을 계속 반복한다.
+
+QuickSort(A, left, right)
+input : array A[left]~A[right]
+Output : sorted array A[left]~A[right]
+
+QuickSort(A, left, p-1) recusively sort the smaller group
+QuickSort(A, p+1, right) recusively sort the larger group
+
+피벗을 선택하는 방법은?
+피벗을 계속적으로 선택한다...!
+
+
+
+
+*/
+
+
+public class QuickSort {
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	
+}
